d154eb24b8f5d6146d85d80320f81f34d18a8a93
Francisco Lopez <franlopbri@gmail.com>
2020-06-05 17:35:29 -0500

    se crean las vistas para mostrar hilos de conversacion

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from pages.urls import pages_patterns
 from django.conf import settings
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
@@ -27,6 +28,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
     path('profiles/', include(profiles_patterns)),
+    path('messenger/', include(messenger_patterns)),
     ]
 
 if settings.DEBUG:
