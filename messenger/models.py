52aca58bea98c47f1542917f438442ead074533a
Francisco Lopez <franlopbri@gmail.com>
2020-06-06 13:40:53 -0500

    Proyecto Finalizado

--- messenger/models.py
+++ messenger/models.py
@@ -28,8 +28,12 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
 
     objects = ThreadManager()
+
+    class Meta:
+        ordering = ['-updated']
 
 def messages_changed(sender, **kwargs):
     instance = kwargs.pop("instance", None)
@@ -47,5 +51,7 @@ def messages_changed(sender, **kwargs):
 
     #Buscar los mensajes de false qye están en el pk_set y se eliminan del set
     pk_set.difference_update(false_pk_set)
+
+    instance.save()
 
 m2m_changed.connect(messages_changed, sender=Thread.messages.through)
